72648078d331f49191b71f19ca81140e1454f644
richardcreatech <richardcreatech@gmail.com>
2026-07-02 01:08:37 +0100

    feat: worked on the part that displays the marketplace for the user

--- server/app/main.py
+++ server/app/main.py
@@ -14,16 +14,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# from app.routes.auth import router as auth_router
-# from app.routes.products import router as product_router
-# from app.routes.marketplace import router as marketplace_router
-# from app.routes.orders import router as order_router
-
-# app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
-# app.include_router(product_router, prefix="/products", tags=["Products"])
-# app.include_router(marketplace_router, prefix="/marketplace", tags=["Marketplace"])
-# app.include_router(order_router, prefix="/orders", tags=["Orders"])
-
 app.include_router(
     auth_router,
     prefix="/auth",
